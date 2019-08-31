22ac190fe15628271414d66a1f49a1925167ba9f
Showrin <showrinece15@gmail.com>
2019-08-31 15:11:25 +0600

    Info Box Added

--- public/javascripts/chat_script.js
+++ public/javascripts/chat_script.js
@@ -10,6 +10,7 @@ $('.user').click(function() {
 
     // changes in navbar
     $('#js_navbar_profile_pic').attr('src', userPicSrc);
+    $('#js_navbar_profile_pic').attr('alt', userName);
     $('#js_navbar_profile_name').text(userName);
     if (userActiveStatus == "\"true\"") {
         $('#js_navbar_active_status').text("Active Now");
@@ -19,4 +20,20 @@ $('.user').click(function() {
         console.log(userActiveStatus)
     }
     
+})
+
+
+$('#js_user_info_btn').click(function() {
+
+    if ($(this).attr('data-active') == 0) {
+        $(this).css({'transform': 'scale(1.1)', 'background-color': '#FFE9EA'});
+        $('#js_chatbox').css('right', '24%');
+        $('#js_chatbox_sendmsg-form').css('right', '24%');
+        $(this).attr('data-active', 1);
+    } else {
+        $(this).css({'transform': 'scale(1)', 'background-color': 'transparent'});
+        $('#js_chatbox').css('right', '0');
+        $('#js_chatbox_sendmsg-form').css('right', '0');
+        $(this).attr('data-active', 0);
+    }
 })
